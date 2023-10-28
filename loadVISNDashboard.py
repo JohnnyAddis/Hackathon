7eff6fb050ee1c738e9ec31ee41b58a0eeecd510
cbarclay25 <cameronrbarclay@gmail.com>
2023-10-28 17:40:28 -0400

    added embedded matplotlib graphs

--- loadVISNDashboard.py
+++ loadVISNDashboard.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
 import sys
+from mplwidget import MplWidget
 
 class loadVISNDashboard(QtWidgets.QMainWindow):
     """Class that loads the Veeva Interactive Sales Navigator
@@ -11,6 +12,21 @@ class loadVISNDashboard(QtWidgets.QMainWindow):
 
         # loads the ui file
         uic.loadUi("VISNDashboard.ui", self)
+
+        self.calendar = self.findChild(QtWidgets.QPushButton,"calendarButton")
+        self.calendar.clicked.connect(self.displayCalendar)
+        self.leads = self.findChild(QtWidgets.QPushButton, "leadsButton")
+        self.leads.clicked.connect(self.displayLeads)
+        
+
+
+        
+
+    def displayCalendar(self):
+        """linked to calendar button, pulls up the interactive calendar feature """
+
+    def displayLeads(self):
+        """linked to leads button, pulls up the window containing a list of sales leads"""
 
 app = QtWidgets.QApplication(sys.argv)
 
